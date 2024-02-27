73acdfefe0539741b1133b291e8df6a92859ad1c
heaodong <adwyyx123@163.com>
2024-02-27 03:13:30 +0000

    finish first detect in notebook

--- Text_correct/CSCDatasets.py
+++ Text_correct/CSCDatasets.py
@@ -1,15 +1,23 @@
+import torch
 from torch.utils.data import Dataset
 
 class DetectionDataset(Dataset):
-    def __init__(self, file):
+    def __init__(self, file, max_len=256):
         super().__init__()
         with open(file, "r", encoding="utf-8") as f:
             self.lines = f.readlines()
+        self.max_len = max_len
         
     def __len__(self):
         return len(self.lines)
 
     def __getitem__(self, i):
-        line = self.lines[i].strip('\n')
-        inputs, labels = line.split('\t')
-        return inputs, labels
+        text, label = self.lines[i].strip('\n').split('\t')
+        label = eval(label)
+        if len(label) > self.max_len - 2:
+            text = text[:self.max_len - 2]
+            label = label[:self.max_len - 2]
+        pad_len = self.max_len - len(text) - 2
+        label = torch.tensor([0] + label + [0] * (pad_len + 1))
+        return text, label
+
--- build/dataset_generation.py
+++ build/dataset_generation.py
@@ -1,9 +1,10 @@
 """用于针对不同任务生成不同数据集"""
 
+import os
 from tqdm import tqdm
 from utils import replace_base_pinyin
 
-def generate_for_Detection(file, data, confuse_data, similar_data):
+def generate_for_Detection(file, data, confuse_data, similar_data, p_confuse: float=0.1, p_same: float=0.6, momentum: float=0.9):
     with open(file, "a", encoding="utf-8") as f:
         for d in tqdm(data):
             confused_text, _, confused_label = replace_base_pinyin(d['correct_text'], confuse_data, similar_data)
@@ -15,14 +16,16 @@ def generate_for_PinyinMLM():
 if __name__ == "__main__":
     import os
     import json
-    dataset_dir = "../SIGHAN2015"
+    dataset_dir = "SIGHAN2015"
     with open(os.path.join(dataset_dir, "train.json"), 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    with open("../data/input/confused_dataset.json", 'r', encoding='utf-8') as f:
+    with open("data/input/confused_dataset.json", 'r', encoding='utf-8') as f:
         confuse_data = json.load(f)
 
-    with open("../data/input/similar_data.json", "r", encoding='utf-8') as f:
+    with open("data/input/similar_data.json", "r", encoding='utf-8') as f:
         similar_data = json.load(f)
     
-    generate_for_Detection("../dataset/train.tsv", data, confuse_data, similar_data)
+    if not os.path.exists("dataset"):
+        os.mkdir("dataset")
+    generate_for_Detection("dataset/train.tsv", data, confuse_data, similar_data, momentum=1)
